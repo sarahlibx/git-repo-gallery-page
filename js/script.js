e09f36a060a7ddf162a5a67865fbbc48fa3d8306
sarah_s <reed.se01@gmail.com>
2024-05-23 21:05:55 -0400

    Create dynamic search

--- js/script.js
+++ js/script.js
@@ -8,6 +8,10 @@ const repoList = document.querySelector(".repo-list");
 const allRepos = document.querySelector(".repos");
 //individual repo data
 const repoData = document.querySelector(".repo-data");
+//back to repo gallery button
+const viewReposButton = document.querySelector(".view-repos");
+//search by name option 
+const filterInput = document.querySelector(".filter-repos"); 
 
 const gitHubProfile = async function () {
     const userInfo = await fetch (`https://api.github.com/users/${username}`);
@@ -31,8 +35,7 @@ const gitUserData = function (data) {
       <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div> `;
     overview.append(div);
-
-   gitRepos(); 
+    gitRepos(username); 
 };
 
 //fetch repos
@@ -44,6 +47,7 @@ const gitRepos = async function () {
 };
 
 const displayRepos = function (repos) {
+    filterInput.classList.remove("hide");
     for (const repo of repos) {
         const repoItem = document.createElement("li");
         repoItem.classList.add("repo");
@@ -91,3 +95,24 @@ const displayRepoInfo = function (repoInfo, languages) {
     `;
     repoData.append(div);
 };
+
+viewReposButton.addEventListener ('click', function () {
+    allRepos.classList.remove("hide");
+    repoData.classList.add("hide");
+    viewReposButton.classList.add("hide");
+});
+
+filterInput.addEventListener ('input', function (e) {
+    const searchText = e.target.value;
+    const repos = document.querySelectorAll(".repo");
+    const searchLowerText = searchText.toLowerCase();
+
+    for (const repo of repos) {
+        const repoLowerText = repo.innerText.toLowerCase();
+        if (repoLowerText.includes(searchLowerText)) {
+          repo.classList.remove("hide");
+        } else {
+          repo.classList.add("hide");
+        }
+      }
+});
